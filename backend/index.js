3290316f48f23f457dce3d91be40b77a44b1ea6b
teghfo <teghfo@gmail.com>
2022-12-22 17:23:15 +0330

    simple chatroom without group chat

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,27 @@ const app = express();
 const path = require("path");
 const http = require("http");
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, "../front")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../front/index.html"));
+});
+
+io.on("connection", (socket) => {
+  socket.on("start", (msg) => {
+    console.log(msg);
+  });
+
+  console.log("ye nafar vasl shod");
+
+  socket.on("disconnect", () => {
+    console.log("ye nafar dis shod");
+  });
+
+  socket.on("message", (msg) => console.log(msg));
 });
 
 server.listen(3000, () => {
--- front/main.js
+++ front/main.js
@@ -1,1 +1,19 @@
-console.log("Salam");
+const socket = io();
+
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const ulElement = document.getElementById("messages");
+
+const username = "Ashkan";
+
+socket.emit("start", username);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const liItem = document.createElement("li");
+  liItem.textContent = input.value;
+  ulElement.appendChild(liItem);
+  socket.emit("message", input.value);
+  window.scrollTo(0, document.body.scrollHeight);
+  input.value = "";
+});
